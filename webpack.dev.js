328e6fcc07c8eea3ff30e4d6c9a0a34f9658a5ad
yezidtozzali <yeezusbroo@gmail.com>
2026-03-06 01:16:15 +0100

    template

---
+++ webpack.dev.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      { test: /\.(png|jpg|jpeg|gif|webp|svg)$/i, type: "asset/resource" },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+  devServer: { static: "./dist", open: true },
+  mode: "development",
+};
